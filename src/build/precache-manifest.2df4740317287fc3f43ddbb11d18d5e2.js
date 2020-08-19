5ff1ead9331c9502aa759c106c80c13dc49590f9
angelle rina <angellerinac@gmail.com>
2020-08-18 23:56:15 -0400

    Initial Commit

---
+++ src/build/precache-manifest.2df4740317287fc3f43ddbb11d18d5e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9709f421288ccc7243e04bb57a1431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8b3e2bc058d542e99fe",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "2898331e29181a2db0f0",
+    "url": "/static/js/2.29ff7c0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29ff7c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b3e2bc058d542e99fe",
+    "url": "/static/js/main.ef5a875f.chunk.js"
+  },
+  {
+    "revision": "caf43f0a14314da69f12",
+    "url": "/static/js/runtime-main.226f7c93.js"
+  }
+]);
